f7abe7cccf82b7029eb20629d664bdc3d3933e02
viharmamania <vihar1903@gmail.com>
2025-04-22 05:28:54 +0000

    Update Users component and fetch logic

---
+++ octofit-tracker/frontend/src/components/Teams.js
@@ -0,0 +1,34 @@
+import React, { useEffect, useState } from 'react';
+
+function Teams() {
+  const [teams, setTeams] = useState([]);
+
+  useEffect(() => {
+    fetch('https://jubilant-capybara-7v5xjj4vr79hprx4-8000.app.github.dev/api/teams/')
+      .then(response => response.json())
+      .then(data => setTeams(data))
+      .catch(error => console.error('Error fetching teams:', error));
+  }, []);
+
+  return (
+    <div className="container">
+      <h1 className="display-4">Teams</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>Team Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {teams.map(team => (
+            <tr key={team._id}>
+              <td>{team.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default Teams;
